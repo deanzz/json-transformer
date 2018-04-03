6c945bfef4863bcb95cae5d0e61a972d01b771b3
Dean.Zhang <dean.zhang@kongming-inc.com>
2018-04-03 15:04:06 +0800

    从系统属性中读取配置文件路径

--- src/main/scala/dean/tools/json/transform/conf/ConfigurationFactory.scala
+++ src/main/scala/dean/tools/json/transform/conf/ConfigurationFactory.scala
@@ -1,14 +1,22 @@
 package dean.tools.json.transform.conf
 
+import java.io.File
+
 import com.typesafe.config.{Config, ConfigFactory}
+
+import scala.util.Try
 
 object ConfigurationFactory {
 
   private var confOpt: Option[Config] = None
 
   def get: Config = {
-    confOpt.getOrElse{
-      val conf = ConfigFactory.load("application.conf")
+    confOpt.getOrElse {
+      val conf = Try {
+        val path = System.getProperty("config.file")
+        println(s"config.file = $path")
+        ConfigFactory.parseFile(new File(path))
+      }.getOrElse(ConfigFactory.load("application.conf"))
       confOpt = Some(conf)
       conf
     }
